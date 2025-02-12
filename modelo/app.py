0037afa7122b8ed59364e4bc5fefad353a032d38
Guifaria12 <gui.faria69@gmail.com>
2025-02-12 20:35:28 -0300

    Update app.py

--- modelo/app.py
+++ modelo/app.py
@@ -68,6 +68,19 @@ base_completa.drop(columns=['INDE 22', 'INDE 2023', 'INDE 23', 'INDE 2024'], inp
 
 ############################# Streamlit ############################
 st.markdown('<style>div[role="listbox"] ul{background-color: #6e42ad}; </style>', unsafe_allow_html=True)
+st.subheader ("""
+**Faculdade de Informática e Administração Paulista – FIAP**
+
+**Grupo 47: Cristiano de Araujo Santos Filho, Eduardo Vilela Silva, Guilherme de Faria
+Rodrigues, Marcelo Pereira Varesi e Vitória Pinheiro de Mattos**
+""")
+st.header("**Introdução**")
+
+st.write("""
+    O preço do petróleo desempenha um papel fundamental na economia global, influenciando mercados financeiros, políticas energéticas e estratégias corporativas em diversos setores, até mesmo em aspectos micro da sociedade (i.e. o preço da gasolina no posto que cotidianamente se abastece o carro para ir ao trabalho). Por ser um dos principais indicadores econômicos, sociais e políticos, compreender sua dinâmica de variação ao longo do tempo é essencial para a tomada de decisões estratégicas em um ambiente econômico volátil e interconectado.
+""")
+
+st.markdown("<hr style='border:1px solid #000000;'>", unsafe_allow_html=True)
 
 st.markdown("<h1 style='text-align: center; '> DATATHON - Modelo Preditivo </h1>", unsafe_allow_html = True)
 
