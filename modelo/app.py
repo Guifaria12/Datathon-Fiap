cb9a5b9f7eea487174bbb68ca2651263530206dd
Guifaria12 <gui.faria69@gmail.com>
2025-02-12 20:36:55 -0300

    Update app.py

--- modelo/app.py
+++ modelo/app.py
@@ -77,10 +77,10 @@ Rodrigues, Marcelo Pereira Varesi e Vitória Pinheiro de Mattos**
 st.header("**Introdução**")
 
 st.write("""
-    O preço do petróleo desempenha um papel fundamental na economia global, influenciando mercados financeiros, políticas energéticas e estratégias corporativas em diversos setores, até mesmo em aspectos micro da sociedade (i.e. o preço da gasolina no posto que cotidianamente se abastece o carro para ir ao trabalho). Por ser um dos principais indicadores econômicos, sociais e políticos, compreender sua dinâmica de variação ao longo do tempo é essencial para a tomada de decisões estratégicas em um ambiente econômico volátil e interconectado.
+A INSERIR
 """)
 
-st.markdown("<hr style='border:1px solid #000000;'>", unsafe_allow_html=True)
+st.markdown("<hr style='border:1px solid #FFFFFF;'>", unsafe_allow_html=True)
 
 st.markdown("<h1 style='text-align: center; '> DATATHON - Modelo Preditivo </h1>", unsafe_allow_html = True)
 
